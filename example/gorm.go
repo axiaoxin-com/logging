974060f3d300f9c1c5a5e96c617177fe5cbf1931
axiaoxin <254606826@qq.com>
2020-04-21 16:57:53 +0800

    update example

--- example/gorm.go
+++ example/gorm.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"os"
+	"sync"
 
 	"github.com/axiaoxin-com/logging"
 	"github.com/jinzhu/gorm"
@@ -19,6 +20,7 @@ type Product struct {
 var (
 	db  *gorm.DB
 	err error
+	wg  sync.WaitGroup
 )
 
 func init() {
@@ -27,8 +29,27 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
+
+	// Migrate the schema
+	db.AutoMigrate(&Product{})
 	// Enable Logger, show detailed log
 	db.LogMode(true)
+
+}
+
+// G 模拟一次请求处理
+func G(traceID string) {
+
+	// Mock a context with a trace id and logger
+	ctx := logging.Context(context.Background(), logging.DefaultLogger(), traceID)
+
+	// 打印带 trace id 的 gorm 日志
+	// 必须先对 db 对象设置带有 trace id 的 ctxlogger 作为 sql 日志打印的 logger
+	// 后续的 gorm 操作使用新的 db 对象即可
+	db := logging.GormDBWithCtxLogger(ctx, db)
+	// Create
+	db.Create(&Product{Code: traceID, Price: 1000})
+	wg.Done()
 }
 
 func main() {
@@ -36,23 +57,17 @@ func main() {
 	defer db.Close()
 	defer os.Remove("./sqlite3.db")
 
-	// Mock a context with a trace id and logger
-	traceID := "logging-fake-trace-id"
-	ctx := logging.Context(context.Background(), logging.DefaultLogger(), traceID)
-
-	// 打印带 trace id 的 gorm 日志
-	// 必须先对 db 对象设置带有 trace id 的 ctxlogger 作为 sql 日志打印的 logger
-	// 后续的 gorm 操作使用新的 db 对象即可
-	ctxLoggerDB := logging.GormDBWithCtxLogger(ctx, db)
-
-	// Migrate the schema
-	ctxLoggerDB.AutoMigrate(&Product{})
-
-	// Create
-	ctxLoggerDB.Create(&Product{Code: "L1212", Price: 1000})
+	// 模拟并发
+	wg.Add(4)
+	go G("g1")
+	go G("g2")
+	go G("g3")
+	go G("g4")
+	wg.Wait()
 }
 
 // log:
-// {"level":"DEBUG","time":"2020-04-20 17:27:55.918425","logger":"root.gorm","msg":"CREATE TABLE \"products\" (\"id\" integer primary key autoincrement,\"created_at\" datetime,\"updated_at\" datetime,\"deleted_at\" datetime,\"code\" varchar(255),\"price\" integer )","pid":79239,"traceID":"logging-fake-trace-id","vars":null,"rowsAffected":0,"duration":0.001237568}
-// {"level":"DEBUG","time":"2020-04-20 17:27:55.919377","logger":"root.gorm","msg":"CREATE INDEX idx_products_deleted_at ON \"products\"(deleted_at) ","pid":79239,"traceID":"logging-fake-trace-id","vars":null,"rowsAffected":0,"duration":0.000748753}
-// {"level":"DEBUG","time":"2020-04-20 17:27:55.919790","logger":"root.gorm","msg":"INSERT INTO \"products\" (\"created_at\",\"updated_at\",\"deleted_at\",\"code\",\"price\") VALUES (?,?,?,?,?)","pid":79239,"traceID":"logging-fake-trace-id","vars":["2020-04-20T17:27:55.919448+08:00","2020-04-20T17:27:55.919448+08:00",null,"L1212",1000],"rowsAffected":1,"duration":0.000332846}
+// {"level":"DEBUG","time":"2020-04-21 17:08:44.449254","logger":"root.gorm","msg":"INSERT INTO \"products\" (\"created_at\",\"updated_at\",\"deleted_at\",\"code\",\"price\") VALUES (?,?,?,?,?)","pid":29826,"traceID":"g4","vars":["2020-04-21T17:08:44.448622+08:00","2020-04-21T17:08:44.448622+08:00",null,"g4",1000],"rowsAffected":1,"duration":0.000613636}
+// {"level":"DEBUG","time":"2020-04-21 17:08:44.452657","logger":"root.gorm","msg":"INSERT INTO \"products\" (\"created_at\",\"updated_at\",\"deleted_at\",\"code\",\"price\") VALUES (?,?,?,?,?)","pid":29826,"traceID":"g2","vars":["2020-04-21T17:08:44.44919+08:00","2020-04-21T17:08:44.44919+08:00",null,"g2",1000],"rowsAffected":1,"duration":0.0034358}
+// {"level":"DEBUG","time":"2020-04-21 17:08:44.458721","logger":"root.gorm","msg":"INSERT INTO \"products\" (\"created_at\",\"updated_at\",\"deleted_at\",\"code\",\"price\") VALUES (?,?,?,?,?)","pid":29826,"traceID":"g1","vars":["2020-04-21T17:08:44.44946+08:00","2020-04-21T17:08:44.44946+08:00",null,"g1",1000],"rowsAffected":1,"duration":0.009227084}
+// {"level":"DEBUG","time":"2020-04-21 17:08:44.471094","logger":"root.gorm","msg":"INSERT INTO \"products\" (\"created_at\",\"updated_at\",\"deleted_at\",\"code\",\"price\") VALUES (?,?,?,?,?)","pid":29826,"traceID":"g3","vars":["2020-04-21T17:08:44.449387+08:00","2020-04-21T17:08:44.449387+08:00",null,"g3",1000],"rowsAffected":1,"duration":0.021678226}
