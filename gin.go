91dfbc5f8c5b36e59df58d5e881872d4c4cf8c36
axiaoxin <254606826@qq.com>
2024-05-30 10:21:29 +0800

    fix get gin req body nil

--- gin.go
+++ gin.go
@@ -4,7 +4,7 @@ import (
 	"bytes"
 	"context"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"net/url"
 	"path"
@@ -413,15 +413,16 @@ func GinLoggerWithConfig(conf GinLoggerConfig) gin.HandlerFunc {
 func GetGinRequestBody(c *gin.Context) []byte {
 	// 获取请求 body
 	var requestBody []byte
-	if c.Request.Body != nil {
-		body, err := ioutil.ReadAll(c.Request.Body)
-		if err != nil {
-			c.Error(err)
-		} else {
-			requestBody = body
-			// body 被 read 、 bind 之后会被置空，需要重置
-			c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(body))
-		}
+	if c == nil || c.Request == nil || c.Request.Body == nil {
+		return requestBody
+	}
+	body, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		c.Error(err)
+	} else {
+		requestBody = body
+		// body 被 read 、 bind 之后会被置空，需要重置
+		c.Request.Body = io.NopCloser(bytes.NewBuffer(body))
 	}
 	return requestBody
 }
